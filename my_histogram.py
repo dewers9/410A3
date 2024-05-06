a78e99203ee030a98360b6a6f226e3e52f25debd
Brandon Vargus <bmv2021@bu.edu>
2024-05-05 21:37:13 -0400

    change names

---
+++ my_histogram.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+# switch to python3 if this is not running ^^^
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+file_types = ["nreg", "ndir", "nblk", "nchr", "nfifo", "nslink", "nsock"]
+
+data = input("")
+
+# split string into numbers
+numbers_str = data.split()
+
+# convert each number from string to integer
+numbers = [int(num) for num in numbers_str]
+print(numbers)
+
+# plot the histogram of the count of files
+plt.bar(file_types, numbers)
+plt.xlabel('File Types')
+plt.ylabel('Frequency')
+plt.title('Frequency of File Typez')
+plt.savefig("my-histogram.jpg")
+plt.show()
